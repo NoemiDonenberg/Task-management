7b38a3aff50b7521925111d50c52714c914ba65c
NoemiDonenberg <n0534182726@gmail.com>
2025-01-28 18:19:33 +0200

    הוספת פונקציונליות להוספת משימות

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Task from "../models/Task";
-// import Task from "../models/Task"; // ייבוא המודל
+import AddTask from "./AddTask";
 
 function TaskList() {
-  // יצירת משימות באמצעות המודל
-  const tasks = [
+  const [tasks, setTasks] = useState([
     new Task(1, "ללמוד React", "2025-01-27"),
     new Task(2, "לסיים פרויקט", "2025-01-28"),
-  ];
+  ]);
+
+  // פונקציה להוספת משימה
+  const addTask = (description) => {
+    const newTask = new Task(
+      tasks.length + 1, // id ייחודי
+      description,
+      new Date().toISOString().split("T")[0] // תאריך נוכחי
+    );
+    setTasks([...tasks, newTask]); // הוספת המשימה למערך
+  };
 
   return (
     <div>
       <h1>רשימת משימות</h1>
+      <AddTask onAddTask={addTask} />
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>
-            <strong>{task.description}</strong> - {task.formatDate()}
+            <strong>{task.description}</strong> - {task.date}
           </li>
         ))}
       </ul>
